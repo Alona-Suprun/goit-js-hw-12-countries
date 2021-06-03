512ed8a68ff667ce005454a3ec48984908e9ef31
Alona-Suprun <a.alonka.l@gmail.com>
2021-06-04 02:00:09 +0300

    Updates

--- src/fetchCountries.js
+++
@@ -1,9 +0,0 @@
-const BASE_URL = "https://restcountries.eu/rest/v2";
-
-const fetchCountries = (countryName) => {
-  return fetch(`${BASE_URL}/name/${countryName}`).then((response) =>
-    response.json()
-  );
-};
-
-export default { fetchCountries };
